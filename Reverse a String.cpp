c9ce3908b8081a0d5c344470a8422a1198b177f1
Deepak Bhatt <deepak.shivay.bhatt@gmail.com>
2021-06-11 22:31:58 +0530

    FINAL_450 #1

--- Reverse a String.cpp
+++ Reverse a String.cpp
@@ -1,31 +1,23 @@
-//Initial Template for C++
-
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 string reverseWord(string str);
 
-int main()
-{
-
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        string s;
-        cin >> s;
-
-        cout << reverseWord(s) << endl;
-    }
-    return 0;
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	string s;
+	cin >> s;	
+	cout << reverseWord(s) << endl;
+	}
+	return 0;
 }
 
-// } Driver Code Ends
-
-//User function Template for C++
-
-string reverseWord(string str)
-{
-    reverse(str.begin(), str.end());
-    return str;
+string reverseWord(string str){
+    int n = (int)str.size();
+    for(int i=n-1; i>=0; i--){
+            cout<<str[i];
+    }
 }
